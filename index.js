9fc870ce5d20142207ee083a4158792dd64f7b90
Hottensiah Wanjiku <w.hottensiah96@gmail.com>
2024-03-30 22:26:18 +0300

    functions continued

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+      return `This Saturday, I want to ${activity}!`;
+};
+ console.log(saturdayFun('sing'));
+
+
+const mondayWork = function(activity='go to the office') {
+    return `This Monday, I will ${activity}.`;
+};
+console.log(mondayWork('learn'));
+
+function wrapAdjective(){
+    // return `$('You are ')*${special}*!`;
+    // return `${"You are "}||${'a dedicated programmer'}||!`;
+    return function (special = "*") {
+        return `${"You are "}*${special}*!`;
+    };   
+  }
+  console.log(wrapAdjective()("a hard worker"));
+ 
+
+
